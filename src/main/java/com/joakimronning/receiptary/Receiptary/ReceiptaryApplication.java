140768c613d11313f56ce1890e70a3b20ce72503
Joakim Rönning <kjronning@gmail.com>
2021-04-07 22:57:30 +0200

    initial commit

---
+++ src/main/java/com/joakimronning/receiptary/Receiptary/ReceiptaryApplication.java
@@ -0,0 +1,13 @@
+package com.joakimronning.receiptary.Receiptary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptaryApplication.class, args);
+	}
+
+}
